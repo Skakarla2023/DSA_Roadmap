2662fe0716f1b9554344096952ea96967c801b9c
Skakarla2023 <skakarla2023@gmail.com>
2025-08-18 07:28:01 +0530

    Update Counting Sort.java

--- 08_Sorting_In_Java/Counting Sort.java
+++ 08_Sorting_In_Java/Counting Sort.java
@@ -6,7 +6,7 @@ public class Ternary_Search {
 
 	public static void main(String[] args) {
 		int arr[] = { 1, 3, 5, 7, 9, 11, 15, 18, 20 };
-		int key = 15;
+		int key = 15; 
 
 		int index = ternarySearch(arr, 0, arr.length - 1, key);
 
